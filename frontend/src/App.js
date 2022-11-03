40d8f3a4c6e2e894d029a1d59d3bc6efd294802f
Daniel Oliveira <danielsapup3@gmail.com>
2022-11-03 15:46:34 -0300

    Update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,31 @@
+import GlobalStyle from "./styles/global";
+import styled from "styled-components";
+import Form from "./componets/Form.js"
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
+const Container = styled.div `
+  width: 100%;
+  max-width: 800px;
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+`;
+
+const Title = styled.h2``; 
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <Container>
+        <Title>Usuários</Title>
+        <Form/>
+      </Container>
+      <ToastContainer autoClose={3000} position={toast.POSITION.BOTTOM_LEFT} />
+      <GlobalStyle/>
+    </>
   );
 }
 
---
+++ frontend/src/components/Form.js
@@ -0,0 +1,61 @@
+import React, { useRef } from "react";
+import styled from "styled-components";
+
+
+const FormContainer = styled.form`
+    display: flex;
+    align-items: flex-end;
+    gap: 10px;
+    flex-wrap: wrap;
+    background-color: #fff;
+    padding: 20px;
+    box-shadow: 0px 0px 5px #ccc;
+    border-radius: 5px;
+`;
+
+const InputArea = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
+const Input = styled.input`
+    width: 120px;
+    padding: 0 10px;
+    border: 1px solid #bbb;
+    border-radius: 5px;
+    height: 40px;
+`;
+
+const Label = styled.label``;
+
+
+const Form = ({ onEdit }) => {
+    const ref = useRef();
+
+    return (
+        <FormContainer ref={ref}>
+            <InputArea>
+                <Label>Nome</Label>
+                <Input name="nome" />
+            </InputArea>
+            <InputArea>
+                <Label>E-mail</Label>
+                <Input name="email" type="email" />
+            </InputArea>
+            <InputArea>
+                <Label>Telefone</Label>
+                <Input name="telefone" />
+            </InputArea>
+            <InputArea>
+                <Label>Data de Nascimento</Label>
+                <Input name="data_nascimento" />
+            </InputArea>
+        </FormContainer>
+    );
+};
+
+export default Form;
+
+
+
+
