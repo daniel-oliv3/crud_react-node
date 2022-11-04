fb5162df1ea8177a84cb0efd4fc57d0d20594ef2
Daniel Oliveira <danielsapup3@gmail.com>
2022-11-04 09:35:00 -0300

    Post

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,3 +1,4 @@
+/* Select */
 import { db } from "../db.js";
 
 export const getUsers = (_, res) => {
@@ -9,3 +10,50 @@ export const getUsers = (_, res) => {
         return res.status(200).json(data);
     });
 };
+
+/* Post */
+export const addUser = (req, res) => {
+    const q = "INSERT INTO usuarios(`nome`, `email`, `telefone`, `data_nascimento`) VALUES(?)";
+
+    const values = [
+        req.body.nome,
+        req.body.email,
+        req.body.telefone,
+        req.body.data_nascimento,
+    ];
+
+    db.query(q, [values], (err) => {
+        if(err) return res.json(err);
+
+        return res.status(200).json("Usuário criado com sucesso!");
+    });
+
+/* Update */
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
